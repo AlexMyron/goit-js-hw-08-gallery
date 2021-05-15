ce99a059e2f288e7a4c0acccbbf254af68b125af
Olek Myron <myrondell@gmail.com>
2021-05-15 08:12:29 +0300

    done with import

--- js/gallery-items.js
+++ js/gallery-items.js
@@ -1,4 +1,4 @@
-// export default
+export {gallery} 
 const gallery = [
   {
     preview:
@@ -67,108 +67,108 @@ const gallery = [
 
 // -----------------------------------------------------
 
-const srcArray = gallery.map(el => el.original);
+// const srcArray = gallery.map(el => el.original);
 
-const galleryContainerEl = document.querySelector('.js-gallery');
-const modalEl = document.querySelector('.js-lightbox');
-const modalImgEl = modalEl.querySelector('.lightbox__image');
-const closeModalBtn = modalEl.querySelector('button[data-action="close-lightbox"]')
-const overlayEl = document.querySelector('.lightbox__overlay')
+// const galleryContainerEl = document.querySelector('.js-gallery');
+// const modalEl = document.querySelector('.js-lightbox');
+// const modalImgEl = modalEl.querySelector('.lightbox__image');
+// const closeModalBtn = modalEl.querySelector('button[data-action="close-lightbox"]')
+// const overlayEl = document.querySelector('.lightbox__overlay')
 
-galleryContainerEl.addEventListener('click', addOriginalLink);
-closeModalBtn.addEventListener('click', onClickBtnModalClose);
-overlayEl.addEventListener('click', onClickOverlayModalClose);
-window.addEventListener('keydown', onKeydownModalClose);
-window.addEventListener('keydown', onKeydownNextImg);
-window.addEventListener('keydown', onKeydownPrewImg)
+// galleryContainerEl.addEventListener('click', addOriginalLink);
+// closeModalBtn.addEventListener('click', onClickBtnModalClose);
+// overlayEl.addEventListener('click', onClickOverlayModalClose);
+// window.addEventListener('keydown', onKeydownModalClose);
+// window.addEventListener('keydown', onKeydownNextImg);
+// window.addEventListener('keydown', onKeydownPrewImg)
 
-renderMarkup(galleryContainerEl, gallery, createMarkup);
+// renderMarkup(galleryContainerEl, gallery, createMarkup);
 
-function createMarkup(array) {
-  return array.map(({ preview, original, description }) => {
-    return `
-    <li class="gallery__item">
-      <a class="gallery__link" href="${original}">
-      <img class="gallery__image" src="${preview}"
-      data-source="${original}" alt="${description}" />
-      </a>
-    </li>
-    `
-  }).join('');
-}
+// function createMarkup(array) {
+//   return array.map(({ preview, original, description }) => {
+//     return `
+//     <li class="gallery__item">
+//       <a class="gallery__link" href="${original}">
+//       <img class="gallery__image" src="${preview}"
+//       data-source="${original}" alt="${description}" />
+//       </a>
+//     </li>
+//     `
+//   }).join('');
+// }
 
-function renderMarkup(el, array, callback) {
-  el.insertAdjacentHTML('beforeend', callback(array))
-}
+// function renderMarkup(el, array, callback) {
+//   el.insertAdjacentHTML('beforeend', callback(array))
+// }
 
-function addOriginalLink(e) {
-  e.preventDefault()
+// function addOriginalLink(e) {
+//   e.preventDefault()
   
-  const img = e.target;
-  const isImage = img.classList.contains('gallery__image')
+//   const img = e.target;
+//   const isImage = img.classList.contains('gallery__image')
 
-  if (!isImage) {    
-    return
-  }
+//   if (!isImage) {    
+//     return
+//   }
 
-  onClickModalOpen(img.dataset.source);
-}
+//   onClickModalOpen(img.dataset.source);
+// }
 
-function onClickModalOpen(source) {
+// function onClickModalOpen(source) {
   
-  modalImgEl.src = '';
-  modalEl.classList.add('is-open');
+//   modalImgEl.src = '';
+//   modalEl.classList.add('is-open');
   
-  modalImgEl.src = source;
+//   modalImgEl.src = source;
   
-}
+// }
 
-function onClickBtnModalClose(e) {
-  modalEl.classList.remove('is-open')
-}
+// function onClickBtnModalClose(e) {
+//   modalEl.classList.remove('is-open')
+// }
 
-function onKeydownModalClose(e) {
-  if (e.code !== 'Escape') {
-    return
-  }
+// function onKeydownModalClose(e) {
+//   if (e.code !== 'Escape') {
+//     return
+//   }
 
-  modalEl.classList.remove('is-open')
-}
+//   modalEl.classList.remove('is-open')
+// }
 
-function onClickOverlayModalClose(e) {
-  if (!e.target.classList.contains('lightbox__overlay')) {
-    return
-  }
-  modalEl.classList.remove('is-open');
-}
+// function onClickOverlayModalClose(e) {
+//   if (!e.target.classList.contains('lightbox__overlay')) {
+//     return
+//   }
+//   modalEl.classList.remove('is-open');
+// }
 
-function onKeydownNextImg(e) {
-  if (e.code !== 'ArrowRight') {
-    return
-  }
+// function onKeydownNextImg(e) {
+//   if (e.code !== 'ArrowRight') {
+//     return
+//   }
   
-  // console.log(modalImgEl.src === srcArray[1])
-  // modalImgEl.src = srcArray[1]
+//   // console.log(modalImgEl.src === srcArray[1])
+//   // modalImgEl.src = srcArray[1]
   
-  for (let i = 0; i < srcArray.length - 1; i += 1) {
-    if (modalImgEl.src === srcArray[i]) {
+//   for (let i = 0; i < srcArray.length - 1; i += 1) {
+//     if (modalImgEl.src === srcArray[i]) {
 
-      return modalImgEl.src = srcArray[i + 1];      
-    }
-  }
-}
+//       return modalImgEl.src = srcArray[i + 1];      
+//     }
+//   }
+// }
 
-function onKeydownPrewImg(e) {
-  if (e.code !== 'ArrowLeft' || modalImgEl.src === srcArray[0]) {
-    return
-  }
+// function onKeydownPrewImg(e) {
+//   if (e.code !== 'ArrowLeft' || modalImgEl.src === srcArray[0]) {
+//     return
+//   }
   
-  for (let i = 0; i < srcArray.length; i += 1) {
+//   for (let i = 0; i < srcArray.length; i += 1) {
 
-    if (modalImgEl.src === srcArray[i]) {
-      modalImgEl.src = '';
-      return modalImgEl.src = srcArray[i - 1];
-    }
-  }
-}
+//     if (modalImgEl.src === srcArray[i]) {
+//       modalImgEl.src = '';
+//       return modalImgEl.src = srcArray[i - 1];
+//     }
+//   }
+// }
 
